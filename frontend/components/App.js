49255802077e6706715e8e06648bbb3461fc612d
Na4d55f <pegunabakr@gmail.com>
2026-04-09 16:48:17 +0530

    Initial commit for frontend App.js

---
+++ frontend/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Music Player</h1>
+            {/* Add your components here */}
+        </div>
+    );
+}
+
+export default App;
